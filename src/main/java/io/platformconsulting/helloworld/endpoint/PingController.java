bc7ddfa9175d286355d1e293331336fdcb108f45
Will Schipp <will.schipp@gmail.com>
2019-07-11 07:14:32 -0500

    initial commit

---
+++ src/main/java/io/platformconsulting/helloworld/endpoint/PingController.java
@@ -0,0 +1,15 @@
+package io.platformconsulting.helloworld.endpoint;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PingController {
+
+    @GetMapping
+    public String ping() {
+        return "pong";
+    }
+}
---
+++ src/test/java/io/platformconsulting/helloworld/ApplicationIT.java
@@ -0,0 +1,25 @@
+package io.platformconsulting.helloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationIT {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    public void main() {
+        assertTrue(applicationContext.getBeanDefinitionCount() > 0);
+    }
+}
